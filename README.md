38d811f3b49168b0aff45325f746ffbff048aa91
Alexey Kasyanchuk <degitx@gmail.com>
2021-08-01 04:39:33 +0300

    update

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ npm install tagslog --save
 
 ```javascript
 // will include global loggin in your application in all js files
-require('tagslog');
+require('tagslog')();
 
 
 // verbose/default message
@@ -30,5 +30,19 @@ logTE('component_bad', 'Error:', 'some error', '(bad)');
 
 <a href="https://github.com/DEgiTx/tag-log"><img src="https://raw.githubusercontent.com/DEgITx/docs/master/tag-log/tag2.png"></a>
 
+## Options
+
+In log constuctor above can be appied options:
+
+```javascript
+// Initialize logging in file beside color log output in console
+require('tagslog')({
+    logFile: 'project.log'
+});
+```
+Options:
+| Option | Description | Default |
+|--------|-------------|---------|
+| logFile | Output log in log file, beside output to console. By default enabled and output to projectName.log from package.json. Can be disabled by logFile: false | projectName.log |
 ## License
 MIT
--- index.js
+++ index.js
@@ -14,30 +14,44 @@ const stringHashCode = (str) => {
 	return hash;
 };
 
-let logFile;
-if(fs.existsSync('./package.json')) {
-	const packageJson = require('./package.json');
-	const projectName = packageJson.name;
-	logFile = fs.createWriteStream(`${projectName}.log`, {flags : 'w'});
-}
+module.exports = (options = {}) => {
+	let logFile;
+	if(options.logFile) {
+		logFile = fs.createWriteStream(options.logFile, {flags : 'w'});
+	} else if (options.logFile !== false && fs.existsSync('./package.json')) {
+		const packageJson = require('./package.json');
+		const projectName = packageJson.name;
+		logFile = fs.createWriteStream(`${projectName}.log`, {flags : 'w'});
+	}
 
-global.logT = (type, ...d) => {
-	const date = (new Date).toLocaleTimeString()
-	console.log(colors.fg.codes[Math.abs(stringHashCode(type)) % 256] + `[${type}]` + colors.reset + ' ' + util.format(...d));
-	if (logFile)
-		logFile.write(`[${date}] ` + util.format(...d) + '\n');
-}
+	global.logT = (type, ...d) => {
+		const date = (new Date).toLocaleTimeString();
+		(options.stdout || console.log)(colors.fg.codes[Math.abs(stringHashCode(type)) % 256] + `[${type}]` + colors.reset + ' ' + util.format(...d));
+		if (logFile)
+			logFile.write(`[${date}] ` + util.format(...d) + '\n');
+	}
 
-global.logTW = (type, ...d) => {
-	const date = (new Date).toLocaleTimeString()
-	console.warn(colors.fg.codes[Math.abs(stringHashCode(type)) % 256] + `[${type}]` + colors.reset + ' ' + colors.fg.codes[11] + util.format(...d) + colors.reset);
-	if (logFile)
-		logFile.write(`[${date}] [WARN] ` + util.format(...d) + '\n');
-}
+	global.logTW = (type, ...d) => {
+		const date = (new Date).toLocaleTimeString();
+		(options.stdout || console.warn)(colors.fg.codes[Math.abs(stringHashCode(type)) % 256] + `[${type}]` + colors.reset + ' ' + colors.fg.codes[11] + util.format(...d) + colors.reset);
+		if (logFile)
+			logFile.write(`[${date}] [WARN] ` + util.format(...d) + '\n');
+	}
 
-global.logTE = (type, ...d) => {
-	const date = (new Date).toLocaleTimeString()
-	console.error(colors.fg.codes[Math.abs(stringHashCode(type)) % 256] + `[${type}]` + colors.reset + ' ' + colors.fg.codes[9] + util.format(...d) + colors.reset);
-	if (logFile)
-		logFile.write(`[${date}] [ERROR] ` + util.format(...d) + '\n');
+	global.logTE = (type, ...d) => {
+		const date = (new Date).toLocaleTimeString();
+		(options.stdout || console.error)(colors.fg.codes[Math.abs(stringHashCode(type)) % 256] + `[${type}]` + colors.reset + ' ' + colors.fg.codes[9] + util.format(...d) + colors.reset);
+		if (logFile)
+			logFile.write(`[${date}] [ERROR] ` + util.format(...d) + '\n');
+	}
+
+	if(options.overrideConsole) {
+		const logPrint = console.log.bind(console);
+		console.log = (...d) => {
+			const date = (new Date).toLocaleTimeString();
+			(options.stdout || logPrint)(util.format(...d));
+			if (logFile)
+				logFile.write(`[${date}] ` + util.format(...d) + '\n');
+		};
+	}
 }
